792012969ac489c9d541605cd9b74ea831685834
Margo Pukhova <margo@puhoff.com>
2024-07-11 12:19:31 +0300

    Рубокоп исправил ошибки

--- app/controllers/telegram/webhook_controller.rb
+++ app/controllers/telegram/webhook_controller.rb
@@ -1,27 +1,29 @@
-class Telegram::WebhookController < Telegram::Bot::UpdatesController
-  def stats!
-    respond_with :message, text: "это команда будет показывать количество сообщений"
-  end
+module Telegram
+  class WebhookController < Telegram::Bot::UpdatesController
+    def stats!
+      respond_with :message, text: "это команда будет показывать количество сообщений"
+    end
 
-  def userstats!
-    respond_with :message, text: "Это команда будет показывать сообщения участника"
-  end
+    def userstats!
+      respond_with :message, text: "Это команда будет показывать сообщения участника"
+    end
 
-  def start!(*)
-    respond_with :message, text: "Начинаю считать сообщения"
-  end
+    def start!(*)
+      respond_with :message, text: "Начинаю считать сообщения"
+    end
 
-  def help!
-    respond_with :message, text: "Здесь будет справка по командам"
-  end
+    def help!
+      respond_with :message, text: "Здесь будет справка по командам"
+    end
 
-  private
+    private
 
-  def locale_for_update
-    if from
-      # locale for user
-    elsif chat
-      # locale for chat
+    def locale_for_update
+      if from
+        # locale for user
+      elsif chat
+        # locale for chat
+      end
     end
   end
 end

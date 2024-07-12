321299626672bb6bd7ce1faf2f08e4ced6cab8d1
Margo Pukhova <margo@puhoff.com>
2024-07-12 19:36:42 +0300

    Рубокоп исправил ошибки

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_12_122744) do
+ActiveRecord::Schema[7.1].define(version: 20_240_712_122_744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,5 +35,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_12_122744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end

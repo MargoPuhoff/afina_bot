1eeccbb0d5d595e15c87e9ff08a50ed7964fa1ee
Margo Pukhova <margo@puhoff.com>
2024-07-13 20:41:49 +0300

    wip аутентификация админа

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:login, :name, :tg_user_id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :timeoutable, :trackable and :omniauthable :registerable, :rememberable
   devise :database_authenticatable, :lockable,
-         :recoverable, :validatable
+         :recoverable, :validatable, authentication_keys: [:login]
+
+  validates :login, presence: true, uniqueness: true
+  validates :tg_user_id, presence: true, uniqueness: true
 end

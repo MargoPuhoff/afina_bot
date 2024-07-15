f56f01a560ae9b2bda3c647f3598dc77c4bd5653
Margo Pukhova <margo@puhoff.com>
2024-07-15 18:37:34 +0300

    Сделала вывод общего количества сообщений и участников

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   http_basic_authenticate_with name: "margo", password: "123123", except: [:logout]
   before_action :set_username, only: [:index]
 
-  def index; end
+  def index
+    @count_tg_message = TgMessage.count
+    @users_with_message_count = TgUser.joins(:tg_messages)
+                                      .select('tg_users.name, COUNT(tg_messages.tg_id) AS messages_count')
+                                      .group('tg_users.tg_id')
+  end
 
   def logout
     reset_session

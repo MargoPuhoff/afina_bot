c273ea5f83cba27d1ff8db18a515c25e3cde0660
Margo Pukhova <margo@puhoff.com>
2024-07-16 16:56:37 +0300

    Сделала div если ни один чат еще не выбран

--- app/javascript/count_message.js
+++ app/javascript/count_message.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Получаю элементы со страницы
   let selectChats = document.getElementById('select_chats')
+  let noSelect = document.getElementById('no_selection')
+  let analyticInfo = document.getElementById('analytic_info')
   let countTgMessage = document.getElementById('count_message')
   let activeUsers = document.getElementById('active_users')
 
   // Обработчик изменения select
   selectChats.addEventListener('change', function() {
     let idChat = selectChats.value
+    noSelect.classList.add('hidden')
+    analyticInfo.classList.remove('hidden')
+
     let request = new XMLHttpRequest();
 
     request.open('GET', '/chats/' + idChat + '/count_tg_message', true)
     request.onload = function() {
 
-      // Обновляю содержимое div с количеством сообщений
+      // Обновляю содержимое div с количеством сообщений и пользователями
       if (request.status >= 200 && request.status < 400) {
         const data = JSON.parse(request.responseText)
         countTgMessage.textContent = data.count_tg_message

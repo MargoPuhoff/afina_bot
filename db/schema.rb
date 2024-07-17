9dde12889dd6222a6faca35bf7e95d623aee9d34
Margo Pukhova <margo@puhoff.com>
2024-07-17 23:15:14 +0300

    Rubocop исправил ошибки

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_125623) do
+ActiveRecord::Schema[7.1].define(version: 20_240_716_125_623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,5 +49,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_125623) do
     t.datetime "updated_at", null: false
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
-
 end
